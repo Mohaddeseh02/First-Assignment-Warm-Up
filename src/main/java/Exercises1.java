360003413994f6491f87d421e074bc10b595eadf
Mohaddeseh_Ganjkhanlou <160696821+Mohaddeseh02@users.noreply.github.com>
2024-02-29 00:05:03 +0330

    a

--- src/main/java/Exercises1.java
+++ src/main/java/Exercises1.java
@@ -1,10 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Exercises1 {
     /*
     implement a function that returns factorial of given n
      */
-    public long factorial(int n) {
-        // TODO
-        return 0;
+    public static long factorial(int n) {
+        int sum = 1;
+        for(int i = n; 0 < i; i--)
+        {
+           sum *= i;
+        }
+        return sum;
     }
 
 
@@ -12,9 +19,12 @@ public class Exercises1 {
     implement a function that return nth number of fibonacci series
     the series -> 1, 1, 2, 3, 5, 8, ...
     */
-    public long fibonacci(int n) {
-        // TODO
-        return 0;
+
+    public static long fibonacci(int n) {
+        if(n == 1 || n == 2) {
+            return 1;
+        }
+        else return fibonacci(n - 2) + fibonacci(n - 1);
     }
 
 
@@ -29,14 +39,19 @@ public class Exercises1 {
     *****
 
      */
-    public char[][] generateTriangle(int rows) {
-        // TODO
-        return null;
+    public static char[][] generateTriangle(int rows) {
+        char[][] triangle = new char[rows][rows];
+        for (int i = 0; i < rows; i++) {
+            triangle[i] = new char[i + 1];
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] = '*';
+            }
+        }
+
+        return triangle;
     }
 
-
     public static void main(String[] args) {
-        // test your code here!
     }
 
 }
